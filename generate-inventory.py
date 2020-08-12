d9eea84be18f7aced9df2b5a16b4626b52f78e5e
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-08-12 20:07:47 +0100

    Update generate-inventory.py

--- generate-inventory.py
+++ generate-inventory.py
@@ -1,7 +1,10 @@
 import ipaddress
 
 print("---")
+
 for num, ip in enumerate(ipaddress.IPv4Network('192.168.2.0/23')):
+    if num == 0:
+        continue
     print(f"R{num}:")
     print("    username: john")
     print("    password: cisco")
