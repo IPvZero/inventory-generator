847a3cf0cf8b1adce90ec77cdbe4c8530b6e6efe
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-08-12 19:58:41 +0100

    Create generate-inventory.py

---
+++ generate-inventory.py
@@ -0,0 +1,11 @@
+import ipaddress
+print("---")
+for num, ip in enumerate(ipaddress.IPv4Network('192.168.2.0/23')):
+    print(f"R{num}:")
+    print("    username: john")
+    print("    password: cisco")
+    print(f"    hostname: {ip}")
+    print("    platform: 'ios'")
+    print("    groups:")
+    print("      - cisco_group")
+    print("\n")
